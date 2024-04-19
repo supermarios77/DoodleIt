7be4d027961894052ad1a49006e9fee1de584ed8
Mariya B <supermariyabros@gmail.com>
2024-04-19 17:36:28 +0300

    Make NextJS Project + Add Start Page UI 🪄

---
+++ doodleit/app/page.tsx
@@ -0,0 +1,26 @@
+import "./ui/HomePage.css";
+
+export default function Home() {
+  return (
+    <main>
+
+      <button className="about-game-btn">
+        ?
+      </button>
+
+      <h1 className="title">Doodle It</h1>
+
+      <p className="text">
+        Experience a thrilling sketching adventure with our TensorFlow-powered
+        app! 🚀🎨 Get a hint, sketch in 30 seconds, and guess the mystery object
+        based on revealed features. No more boring prompts – just creative fun!
+        🕵️‍♂️🎢🚀🎨
+      </p>
+
+      <button className="start-game-btn">
+        Let&apos;s Draw!
+      </button>
+
+    </main>
+  );
+}
---
+++ doodleit/app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Delicious_Handrawn } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const DelicousHandrawn = Delicious_Handrawn({ weight: ["400"] , subsets: ["latin"]})
